346c844024fea0cc551412ad1eb2a8e6c8c11e56
Sergio Tenza <43845783+SergioTenza@users.noreply.github.com>
2020-12-10 18:13:19 +0100

    Recarga en Github
    Primer Commit

---
+++ FindHandle/DesktopWindowsStuff.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using System.Runtime.InteropServices;
+
+namespace FindHandle
+{
+    static class DesktopWindowsStuff
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd,
+            StringBuilder lpWindowText, int nMaxCount);
+
+        [DllImport("user32.dll", EntryPoint = "EnumDesktopWindows",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool EnumDesktopWindows(IntPtr hDesktop,
+            EnumDelegate lpEnumCallbackFunction, IntPtr lParam);
+
+        // Define the callback delegate's type.
+        private delegate bool EnumDelegate(IntPtr hWnd, int lParam);
+
+        // Save window titles and handles in these lists.
+        private static List<IntPtr> WindowHandles;
+        private static List<string> WindowTitles;
+
+        // Return a list of the desktop windows' handles and titles.
+        public static void GetDesktopWindowHandlesAndTitles(
+            out List<IntPtr> handles, out List<string> titles)
+        {
+            WindowHandles = new List<IntPtr>();
+            WindowTitles = new List<string>();
+
+            if (!EnumDesktopWindows(IntPtr.Zero, FilterCallback, IntPtr.Zero))
+            {
+                handles = null;
+                titles = null;
+            }
+            else
+            {
+                handles = WindowHandles;
+                titles = WindowTitles;
+            }
+        }
+
+        // We use this function to filter windows.
+        // This version selects visible windows that have titles.
+        private static bool FilterCallback(IntPtr hWnd, int lParam)
+        {
+            // Get the window's title.
+            StringBuilder sb_title = new StringBuilder(1024);
+            int length = GetWindowText(hWnd, sb_title, sb_title.Capacity);
+            string title = sb_title.ToString();
+
+
+            // If the window is visible and has a title, save it.
+            if (IsWindowVisible(hWnd) && string.IsNullOrEmpty(title) == false)
+            {
+
+                WindowHandles.Add(hWnd);
+                Console.WriteLine("HANDLE :  " + hWnd.ToString());
+                WindowTitles.Add(title);
+                Console.WriteLine("TITULO :  " + title);
+                Console.WriteLine("============================================================================");
+                // Define path of text file to write
+                string path = @"c:\Temp\Handles.txt";
+
+                if (!File.Exists(path))
+                {
+                    // Create a file to write to. 
+                    string[] createTextHandle = { title, hWnd.ToString() };
+                    File.WriteAllLines(path, createTextHandle, Encoding.UTF8);
+                }
+                else
+                {
+                    // This text is always added, making the file longer over time
+                    // if it is not deleted.
+                    string appendText = title + Environment.NewLine + hWnd.ToString() + Environment.NewLine;
+                    File.AppendAllText(path, appendText, Encoding.UTF8);
+                }
+
+            }
+
+            // Return true to indicate that we
+            // should continue enumerating windows.
+            return true;
+        }
+    }
+}
---
+++ FindHandle/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+using System.IO;
+
+namespace FindHandle
+{
+    class Program
+    {
+        // Default folder    
+        static readonly string rootFolder = @"C:\Temp\";
+
+        static void Main(string[] args)
+        {
+            // File to be deleted    
+            string authorsFile = "Handles.txt";
+
+            try
+            {
+                // Check if file exists with its full path    
+                if (File.Exists(Path.Combine(rootFolder, authorsFile)))
+                {
+                    // If file found, delete it    
+                    File.Delete(Path.Combine(rootFolder, authorsFile));
+                    Console.WriteLine("File Handles.txt removed...");
+                }
+                else Console.WriteLine("File Handles.txt not present creating new one on: "+rootFolder);
+            }
+            catch (IOException ioExp)
+            {
+                Console.WriteLine(ioExp.Message);
+            }
+            Console.WriteLine("============================================================================");
+            Console.WriteLine("============================================================================");
+            Console.WriteLine("Searching Titles and Handles of all Windows, Please wait...");
+            Console.WriteLine("============================================================================");
+            ShowDesktopWindows();
+            Console.WriteLine("Task completed.");            
+            Console.WriteLine("============================================================================");
+            Console.WriteLine("============================================================================");
+            Console.WriteLine("============================================================================");            
+            Console.WriteLine("Task completed. 5 seconds to close app.");
+            Console.WriteLine("============================================================================");
+            Console.WriteLine("============================================================================");            
+            Console.WriteLine("============================================================================");
+            Thread.Sleep(5000);
+            Environment.Exit(0);
+            ///Console.ReadLine();
+
+
+
+            //    string _handle = SearchHandle("obs").ToString();
+            //    string _handle2 = SearchHandle("OBS").ToString();
+            //    string _handle3 = SearchHandle("obs64").ToString();
+
+            //    Console.WriteLine("  Opcion obs: " + _handle + "  Opcion OBS: " + _handle2 + "  Opcion obs64: " + _handle3 + "\r\n");
+
+            //    Thread.Sleep(5000);
+
+            //    string _handle4 = SearchHandle("obs").ToString();
+            //    string _handle5 = SearchHandle("OBS").ToString();
+            //    string _handle6 = SearchHandle("obs64").ToString();
+
+            //    Console.WriteLine("  Opcion obs: " + _handle4 + "  Opcion OBS: " + _handle5 + "  Opcion obs64: " + _handle6 + "\r\n");
+
+            //    Thread.Sleep(5000);
+
+            //    Console.WriteLine("Tareas Finalizadas.");
+
+            //    Console.ReadLine();
+            //}
+             
+        }
+        // Display a list of the desktop windows' titles.
+        private static void ShowDesktopWindows()
+        {
+            List<IntPtr> handles;
+            List<string> titles;
+            DesktopWindowsStuff.GetDesktopWindowHandlesAndTitles(out handles, out titles);
+        }
+        
+    }
+}
